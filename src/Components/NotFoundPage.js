843c735cd912be2b5de2c222e50fd871059cf8a8
Johan <johan@romantech.net>
2021-11-11 21:29:38 +0900

    :children_crossing: Update: NotFoundPage 타이머 추가

--- src/Components/NotFoundPage.js
+++ src/Components/NotFoundPage.js
@@ -1,15 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
 import { Result, Button } from 'antd';
 import { ContainerStyle } from '../Styles/commonStyles';
 
 const NotFoundPage = function ({ history }) {
+  const [seconds, setSeconds] = useState(5);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds(seconds - 1);
+      if (seconds === 1) history.push('/home');
+    }, 1000);
+
+    return () => clearInterval(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [seconds]);
+
   return (
     <Container>
       <Result
         status="404"
         title="페이지를 찾을 수 없어요!"
+        subTitle={`${seconds}초 뒤 홈 화면으로 이동합니다`}
         extra={
           <Button type="primary" onClick={() => history.push('/home')}>
             Back Home
