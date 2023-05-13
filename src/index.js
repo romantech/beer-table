147746c9306786db4f1d87e3d096ee26ea7cddb2
ColorFilter <johan@romantech.net>
2023-05-13 16:22:08 +0900

    🐛 fix(.env.example): add GENERATE_SOURCEMAP=false to fix source map parsing issue 🔧 chore(package.json): update dependencies and fix material-table version to 1.69.3 🐛 fix(index.js): replace ReactDOM.render with root.render to fix React 18 compatibility issue and update antd.css to antd.min.css The .env.example file now includes the GENERATE_SOURCEMAP=false variable to fix the source map parsing issue. The package.json file has been updated to fix the material-table version to 1.69.3 and update other dependencies. The index.js file has been updated to replace ReactDOM.render with root.render to fix the React 18 compatibility issue and update antd.css to antd.min.css.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore } from 'redux-persist';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { ThemeProvider } from 'styled-components/macro';
+import { createRoot } from 'react-dom/client';
 import theme from './Styles/theme';
 import GlobalStyle from './Styles/globalStyle';
 import Routes from './Routes';
 import rootReducer, { rootSaga } from './Modules';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
 
 const sagaMiddleware = createSagaMiddleware();
 const enhancer =
@@ -24,7 +28,7 @@ const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <ThemeProvider theme={theme}>
@@ -33,5 +37,4 @@ ReactDOM.render(
       </ThemeProvider>
     </PersistGate>
   </Provider>,
-  document.getElementById('root'),
 );
